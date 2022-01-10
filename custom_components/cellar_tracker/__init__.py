a0cb7feec6324d8cca03a8847a404b77dcbebf67
Maarten Mees <26173684+MaartenMees@users.noreply.github.com>
2022-01-10 06:44:58 +0100

    Update __init__.py

--- custom_components/cellar_tracker/__init__.py
+++ custom_components/cellar_tracker/__init__.py
@@ -93,16 +93,17 @@ class WineCellarData:
       groups = ['Varietal', 'Country', 'Vintage', 'Producer', 'Type', 'Size', 'Bin']
 
       for group in groups:
-        group_data = df.groupby(group).agg({'iWine':'count','Valuation':['sum','mean']},'Vintage')
+        group_data = df.groupby(group).agg({'iWine':'count','Valuation':['sum','mean']})
         group_data.columns = group_data.columns.droplevel(0)
         group_data["%"] = 1
         group_data["%"] = (group_data['count']/group_data['count'].sum() ) * 100
-        group_data.columns = ["count", "value_total", "value_avg", "%", "Bin", "Size", "Vintage"]
+        group_data.columns = ["count", "value_total", "value_avg", "%"]
         data[group] = {}
         for row, item in group_data.iterrows():
           data[group][row] = item.to_dict()
           data[group][row]["sub_type"] = row
             
+    
       data["total_bottles"] = len(df)
       data["total_value"] = df['Valuation'].sum()
       data["average_value"] = df['Valuation'].mean()
