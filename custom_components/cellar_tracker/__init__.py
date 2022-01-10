717199ca17ebbeffacba2dd349f2aa76e2c880a8
Maarten Mees <26173684+MaartenMees@users.noreply.github.com>
2022-01-10 09:13:00 +0100

    Update __init__.py

--- custom_components/cellar_tracker/__init__.py
+++ custom_components/cellar_tracker/__init__.py
@@ -93,13 +93,13 @@ class WineCellarData:
       groups = ['Varietal', 'Country', 'Vintage', 'Producer', 'Type']
 
       for group in groups:
-        group_data = df.groupby(group).agg({'iWine','Valuation':['sum','mean']})
+        group_data = df.groupby(group).agg({'iWine':'count','Valuation':['sum','mean'],'Size','Bin'})
         group_data.columns = group_data.columns.droplevel(0)
         group_data["%"] = 1
         group_data["%"] = (group_data['count']/group_data['count'].sum() ) * 100
         group_data["bottle_size"] = df['Size']
         group_data["cellar_location"] = df['Bin']
-        group_data.columns = ["count", "value_total", "value_avg", "%"]
+        group_data.columns = ["count", "value_total", "value_avg", "%", "bottle_size", "cellar_location"]
         data[group] = {}
         for row, item in group_data.iterrows():
           data[group][row] = item.to_dict()
